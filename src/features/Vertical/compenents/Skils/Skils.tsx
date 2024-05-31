0634335e89c29a36542f7f2c9f795e56d8c1bc76
Miyagoshi_Sota <k23122kk@aitech.ac.jp>
2024-05-31 15:42:55 +0900

    feat: skilsを追加

--- src/features/Vertical/compenents/Skils/Skils.tsx
+++ src/features/Vertical/compenents/Skils/Skils.tsx
@@ -12,6 +12,12 @@ import { FaReact } from "react-icons/fa";
 import { SiNestjs } from "react-icons/si";
 import { IoLogoFirebase } from "react-icons/io5";
 import { FaUnity } from "react-icons/fa";
+import { DiDjango } from "react-icons/di";
+import { FaPython } from "react-icons/fa";
+import { FaFigma } from "react-icons/fa";
+import { SiOpencv } from "react-icons/si";
+import { DiMysql } from "react-icons/di";
+import { IoLogoElectron } from "react-icons/io5";
 
 const Skils = () => {
   return (
@@ -21,10 +27,16 @@ const Skils = () => {
         <TechCard icon={<FaHtml5 size={"10vh"} />} name="HTML" />
         <TechCard icon={<FaCss3Alt size={"10vh"} />} name="CSS" />
         <TechCard icon={<FaReact size={"10vh"} />} name="React" />
+        <TechCard icon={<IoLogoElectron size={"10vh"} />} name="Electron" />
         <TechCard icon={<SiNestjs size={"10vh"} />} name="Nestjs" />
-        <TechCard icon={<FaGithub size={"10vh"} />} name="GitHubs" />
+        <TechCard icon={<FaPython size={"10vh"} />} name="Python" />
+        <TechCard icon={<DiDjango size={"10vh"} />} name="Django" />
+        <TechCard icon={<SiOpencv size={"10vh"} />} name="Opencv" />
+        <TechCard icon={<DiMysql size={"10vh"} />} name="Mysql" />
         <TechCard icon={<IoLogoFirebase size={"10vh"} />} name="Firebase" />
         <TechCard icon={<FaUnity size={"10vh"} />} name="Unity" />
+        <TechCard icon={<FaFigma size={"10vh"} />} name="figma" />
+        <TechCard icon={<FaGithub size={"10vh"} />} name="GitHub" />
       </div>
     </div>
   );
