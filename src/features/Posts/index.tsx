961dfe3152a6a482d8937c29c214771f7d554f29
Miyagoshi_Sota <k23122kk@aitech.ac.jp>
2024-11-12 19:45:48 +0900

    clean:delete toplevel await

--- src/features/Posts/index.tsx
+++ src/features/Posts/index.tsx
@@ -1,23 +1,35 @@
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
-//components
+// components
 import Title from "../../components/Title/Title";
 import PostCard from "../../components/Cards/PostCard";
-import { getBlogs } from "./components/postsLogic";
-
-const { contents: posts } = await getBlogs();
+import { getBlogs, Blog } from "./components/postsLogic";
 
 const Posts = () => {
+  const [posts, setPosts] = useState<Blog[]>();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const { contents } = await getBlogs();
+      setPosts(contents);
+    };
+    fetchPosts();
+  }, []);
 
   return (
     <div className={styles.posts_boxs}>
       <Title text="Posts" />
       <div className={styles.posts_grid}>
-        {
-          posts.map((post) => (
-            <PostCard id={post.id}  src={post.eyecatch.url} title={post.title} day={post.category.createdAt} />
-          ))
-        }
+        {posts?.map((post) => (
+          <PostCard
+            key={post.id}
+            id={post.id}
+            src={post.eyecatch.url}
+            title={post.title}
+            day={post.category.createdAt}
+          />
+        ))}
       </div>
     </div>
   );
