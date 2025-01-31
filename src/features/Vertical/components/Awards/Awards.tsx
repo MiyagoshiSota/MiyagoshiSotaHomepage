b9f4d7155981a9ed88e3890620b65c3e40bc243e
MiyagoshiSota <130554970+MiyagoshiSota@users.noreply.github.com>
2025-02-01 00:01:51 +0900

    Update Awards.tsx

--- src/features/Vertical/components/Awards/Awards.tsx
+++ src/features/Vertical/components/Awards/Awards.tsx
@@ -9,10 +9,11 @@ const Awards = () => {
     <div className={styles.awards}>
       <Title text={"Awards"} />
       <div className={styles.cards}>
-        <AwardCard title = {"[最優秀賞] 2023.06"} event={"技育CAMP キャラバンハッカソン vol.2"} product={"紙アプリ"} url="./"/>
-        <AwardCard title = {"[企業賞] 2023.9"} event={"HackAichi2023"} product={"社食の革命"} url="./"/>
-        <AwardCard title = {"[優秀賞] 2023.9"} event={"HackSummer東海2023"} product={"おしゃべりずんだもん"} url="./"/>
-        <AwardCard title = {"[WingArc1st賞] 2023.11"} event={"技育CAMP キャラバンハッカソン vol.4"} product={"YOFUKASHI"} url="./"/>
+        <AwardCard title = {"[最優秀賞] 2023.06"} event={"技育CAMP キャラバンハッカソン vol.2"} product={"紙アプリ"} url="https://www.marmot-kawaii.net/posts/q9lq47qx9x86"/>
+        <AwardCard title = {"[企業賞] 2023.9"} event={"HackAichi2023"} product={"社食の革命"} url="https://www.marmot-kawaii.net/posts/gpyi0y_0flt"/>
+        <AwardCard title = {"[優秀賞] 2023.9"} event={"HackSummer東海2023"} product={"おしゃべりずんだもん"} url="https://www.marmot-kawaii.net/posts/n7v5a6gwqoci"/>
+        <AwardCard title = {"[WingArc1st賞] 2023.11"} event={"技育CAMP キャラバンハッカソン vol.4"} product={"YOFUKASHI"} url="https://www.marmot-kawaii.net/posts/re7y3hwdw"/>
+        <AwardCard title = {"[優秀賞] 2024.11"} event={"技育CAMP Vol.16"} product={"目覚まし黒電話"} url="https://www.marmot-kawaii.net/posts/nug_-x_efq1v"/>
       </div>
     </div>
   );
