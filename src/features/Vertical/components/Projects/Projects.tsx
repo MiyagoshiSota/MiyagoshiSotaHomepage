da4abf54ade57ce1bc3ccdd9432e22d43b35ec8b
MiyagoshiSota <130554970+MiyagoshiSota@users.noreply.github.com>
2025-01-31 23:49:05 +0900

    Update Projects.tsx

--- src/features/Vertical/components/Projects/Projects.tsx
+++ src/features/Vertical/components/Projects/Projects.tsx
@@ -26,37 +26,37 @@ const Projects = () => {
           <ul className={styles.first}>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/1jk6musspzt"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/63c449686faa428cbd4d2202b4632b82/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-20%2017.16.52.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/xdh2qj-pj"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/dd45c1a4112148be8d2c5c438552f780/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-20%2017.01.52.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/z4pv2lt3vz"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/0044d4948b1544f4bffa47c7a2d3dcbd/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-20%2016.40.56.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/xhrp142x5"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/e7d6da8f06004245b634c32d5feb3ce6/_geek-camp2023-caravan-val2._006.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/3dreuvz20zlz"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/413b50a03c5f4e20b3e61f689ff21daf/0da7c24b-2302-454b-87d6-81f299a0.webp"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/4_387vcrz1"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/78434063e8834bd694b36fcbbe0d5f8b/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-26%2017.44.58.png"
               />
             </li>
@@ -64,37 +64,37 @@ const Projects = () => {
           <ul className={styles.first}>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/1jk6musspzt"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/63c449686faa428cbd4d2202b4632b82/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-20%2017.16.52.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/xdh2qj-pj"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/dd45c1a4112148be8d2c5c438552f780/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-20%2017.01.52.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/z4pv2lt3vz"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/0044d4948b1544f4bffa47c7a2d3dcbd/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-20%2016.40.56.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/xhrp142x5"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/e7d6da8f06004245b634c32d5feb3ce6/_geek-camp2023-caravan-val2._006.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/3dreuvz20zlz"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/413b50a03c5f4e20b3e61f689ff21daf/0da7c24b-2302-454b-87d6-81f299a0.webp"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/4_387vcrz1"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/78434063e8834bd694b36fcbbe0d5f8b/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-26%2017.44.58.png"
               />
             </li>
@@ -104,37 +104,37 @@ const Projects = () => {
           <ul className={styles.second}>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/dey2hhqr03op"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/74dd810e69bf4b2cac4b40f18e3d9071/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-24%2015.31.03.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/eogme3crobw"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/30b104c0109c4211b7df66491be00921/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-24%2015.36.56.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/7ylf319lwy"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/4b1da357688c4226ab3b5a39707965ce/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-24%2015.41.40.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/psi04-20go"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/deb57bdcd1a4438dae631a4586b993bf/47add3af5de5519d.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/vvbohc5u2v"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/c467d1b616904a0d820e02f83d6cd0aa/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-24%2015.59.39.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/r9vhgcts-o"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/7cd2d73da7dc40e5a61a2c5508e729d5/%E5%9B%B31.png"
               />
             </li>
@@ -142,37 +142,37 @@ const Projects = () => {
           <ul className={styles.second}>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/dey2hhqr03op"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/74dd810e69bf4b2cac4b40f18e3d9071/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-24%2015.31.03.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/eogme3crobw"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/30b104c0109c4211b7df66491be00921/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-24%2015.36.56.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/7ylf319lwy"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/4b1da357688c4226ab3b5a39707965ce/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-24%2015.41.40.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/psi04-20go"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/deb57bdcd1a4438dae631a4586b993bf/47add3af5de5519d.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/vvbohc5u2v"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/c467d1b616904a0d820e02f83d6cd0aa/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202024-11-24%2015.59.39.png"
               />
             </li>
             <li className={styles.scroll_item}>
               <ProductCard
-                url=""
+                url="https://miyagoshi-sota-git-main-miyagoshis-projects.vercel.app/posts/r9vhgcts-o"
                 src="https://images.microcms-assets.io/assets/5d98dcaed7af47fdb5252aa891fa552a/7cd2d73da7dc40e5a61a2c5508e729d5/%E5%9B%B31.png"
               />
             </li>
